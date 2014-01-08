0530e67c6f93db4b2dcd145e71df6844dbc5232f
hbhunt <bhunt@osx-bhunt-air.simpletuition.local>
2014-01-08 13:38:10 -0500

    Finished sweet railscast tutorial for adding password reset emails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  get "password_resets/new"
   root 'static_pages#home'
 
   resources :users do
@@ -9,6 +10,7 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :password_resets
   
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106002444) do
+ActiveRecord::Schema.define(version: 20140108161852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20140106002444) do
     t.text     "institution_type"
     t.boolean  "has_graduated"
     t.boolean  "has_full_time_employment"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
